1155c7c9bf5b5804804074d4b1d751d383ac3861
likusta <anikolaev.rudn@yandex.ru>
2020-02-24 21:02:08 +0300

    homework_1

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/NotificationMessageGeneratorImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/NotificationMessageGeneratorImpl.kt
@@ -1,12 +1,18 @@
 package ru.tinkoff.fintech.service.notification
 
 import ru.tinkoff.fintech.model.NotificationMessageInfo
+import java.time.format.DateTimeFormatter
 
 class NotificationMessageGeneratorImpl(
     private val cardNumberMasker: CardNumberMasker
 ) : NotificationMessageGenerator {
 
     override fun generateMessage(notificationMessageInfo: NotificationMessageInfo): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return "Уважаемый, " + notificationMessageInfo.name + "!\n" +
+                "Спешим Вам сообщить, что на карту " +
+                cardNumberMasker.mask(notificationMessageInfo.cardNumber, '#', 0, 12) +
+                " начислен cashback в размере " + notificationMessageInfo.cashback.toString() +
+                " за категорию " + notificationMessageInfo.category + ".\nСпасибо за покупку " + notificationMessageInfo.transactionDate.format( DateTimeFormatter.ofPattern("yyyy-MM-ddEHH:mm"))
+
     }
 }
